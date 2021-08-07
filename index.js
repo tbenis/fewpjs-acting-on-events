010566fa0e6286c5d9582b7b79a39a506879d608
Benis Tambe <will.jenifer@yahoo.com>
2021-08-07 13:18:06 -0400

    commit tree 18ed78f3789df3683e93c4dfe7627eea09410a9f

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+
+let dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+dodger.style.bottom = "0px";
+
+document.addEventListener("keydown", function (e) {
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+      }
+  });
+});
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+function moveDodgerRight() {
+    
+  var leftNumbers = dodger.style.left.replace('px', '');
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
